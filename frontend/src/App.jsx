756bc180effeb9ce801fa1ef5b278ea3f1cfe7a2
Ibrahim Osmanoglu <osmanoglu.ibrahim.tr@gmail.com>
2026-09-15 10:10:56 +0300

    feat: add data check on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,81 @@ import './App.css'
 
 
 function App() {
-  const [expenses, setExpenses] = useState(JSON.parse(localStorage.getItem('expense')) || [])
-  const [incomes, setIncomes] = useState(JSON.parse(localStorage.getItem('income')) || [])
+
+  function isValid(data) {
+    if (!Array.isArray(data)) return false
+    const requiredProperties = ['description', 'amount', 'categoryId', 'time', 'date', 'id']
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+    const timeRegex = /^\d{2}:\d{2}$/
+
+
+    function dateOk({ item, property }) {
+      if (!dateRegex.test(item[property])) return false
+
+      const parts = item[property].split("-")
+      const year = Number(parts[0])
+      const month = Number(parts[1])
+      const day = Number(parts[2])
+
+      const newDate = new Date(year, month - 1, day)
+      if (
+        newDate.getFullYear() !== year
+        || newDate.getMonth() !== month - 1
+        || newDate.getDate() !== day
+      ) return false
+
+      return true
+    }
+
+
+    function timeOk({ item, property }) {
+      if (!timeRegex.test(item[property])) return false
+      const parts = item[property].split(":")
+      const hour = Number(parts[0])
+      const minute = Number(parts[1])
+
+      if (hour > 23 || hour < 0 || minute > 59 || minute < 0) return false
+
+      return true
+    }
+
+
+
+
+    return data.every(item => {
+      if ((typeof item !== 'object') || (item === null)) return false
+
+      if (Object.keys(item).length !== requiredProperties.length) return false
+
+      return requiredProperties.every(property => {
+        return (
+          (property in item)
+          && (
+            (property === 'date' && dateOk({ item, property }))
+            || (property === 'time' && timeOk({ item, property }))
+            || (property === 'amount' && Number.isFinite(item[property]))
+            || (property !== 'amount' && property !== 'date' && property !== 'time' && typeof item[property] === 'string' && item[property].trim() !== '')
+          )
+        )
+      })
+    })
+  }
+
+  function getStoredData(list) {
+    try {
+      const data = JSON.parse(localStorage.getItem(list))
+      if (!isValid(data)) {
+        throw new Error(`${list} data is not valid`)
+      }
+      return data
+    } catch (error) {
+      console.log(error)
+      return []
+    }
+  }
+
+  const [expenses, setExpenses] = useState(() => getStoredData('expense'))
+  const [incomes, setIncomes] = useState(() => getStoredData('income'))
 
   const totalExpenses = expenses.reduce((acc, expense) => acc + expense.amount, 0)
   const totalIncomes = incomes.reduce((acc, income) => acc + income.amount, 0)
